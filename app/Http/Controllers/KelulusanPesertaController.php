d61a47e93deb9479f2caea375f71e2c4a3695a7b
rafimaryudwika <northernosprey2013@gmail.com>
2023-08-29 01:00:41 +0700

    Tambah controller

---
+++ app/Http/Controllers/KelulusanPesertaController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\KelulusanPeserta;
+use App\Http\Requests\StoreKelulusanPesertaRequest;
+use App\Http\Requests\UpdateKelulusanPesertaRequest;
+
+class KelulusanPesertaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKelulusanPesertaRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(KelulusanPeserta $kelulusanPeserta)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(KelulusanPeserta $kelulusanPeserta)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKelulusanPesertaRequest $request, KelulusanPeserta $kelulusanPeserta)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(KelulusanPeserta $kelulusanPeserta)
+    {
+        //
+    }
+}
