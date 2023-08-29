cc91c23c8efb6c5d95c64c89a1b6af2af813d9ae
rafimaryudwika <northernosprey2013@gmail.com>
2023-08-29 23:06:21 +0700

    Untuk konfigurasi

---
+++ app/Http/Controllers/KonfigurasiController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Konfigurasi;
+use App\Http\Requests\StoreKonfigurasiRequest;
+use App\Http\Requests\UpdateKonfigurasiRequest;
+
+class KonfigurasiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreKonfigurasiRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Konfigurasi $konfigurasi)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Konfigurasi $konfigurasi)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateKonfigurasiRequest $request, Konfigurasi $konfigurasi)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Konfigurasi $konfigurasi)
+    {
+        //
+    }
+}
